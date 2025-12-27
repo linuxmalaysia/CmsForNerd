880a1c7f94a3be8387afaeedb01b8ee77a72fa0f
Harisfazillah Jamel <linuxmalaysia@songketmail.org>
2025-12-27 14:38:10 +0800

    Compliance: Added a standards test and fixed strict_types in includes/index.php

--- includes/index.php
+++ includes/index.php
@@ -1,9 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 // Redirect people who try to view this folder to the main index.php page
 // CmsForNerd
 
-header("Location:../index.php");
+header("Location: ../index.php");
+
 
 ?>
 
