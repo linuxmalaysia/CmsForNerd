544bca801f3f4d1738d32aa88105715dc0c7c6f0
Harisfazillah Jamel <linuxmalaysia@songketmail.org>
2025-12-27 16:46:41 +0800

    Docs: Updated Template Guide and improved template.php instructional content

--- template.php
+++ template.php
@@ -1,30 +1,11 @@
-<?php
-
-// This is the script that give the file name without extension .php
-// and pass it to others.
-
-// CmsForNerd 
-// Idea from drupal and xampp and some codes from them
-// License GNU Public License V2
-// Harisfazillah Jamel v 1.1 7 Feb 2006 linuxmalaysia @ gmail dot com
-// Harisfazillah Jamel v 1.2 18 November 2007 linuxmalaysia @ gmail dot com
-// Harisfazillah Jamel v 3.1 27 Dec 2025
-// With the help of Google Gemini
+// [BOILERPLATE] template.php - The base foundation for all Nerd-Stack pages.
 //
-// FILE PURPOSE:
-// Template / Boilerplate. This file:
-// 1. Serves as a skeleton for creating new custom pages.
-// 2. demonstrates how to initialize the CMS Context and load the theme.
-// For https://www.linuxmalaysia.com
-// https://www.linuxmalaysia.com/
-// For small site without database just pure php html xml code
-// Remember all page copy this and please check the local
-// theme or lang overwrite
-
-// The only thing need to change is the Title
-
-// The contents of the page will be in contents/
-// and with name ???-body.inc
+// HOW TO USE:
+// 1. Copy this file to a new name (e.g., about.php).
+// 2. Create a corresponding content file (e.g., contents/about-body.inc).
+// 3. Customize the metadata in the $CONTENT array below.
+//
+// RFC 2119: You MUST NOT modify the core logic here unless you are a "Theme Engineer".
 
 ob_start("ob_gzhandler");
 require_once __DIR__ . '/vendor/autoload.php';
