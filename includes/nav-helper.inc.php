c8f171c8b18f63f082469e0462974656324cb1ea
Harisfazillah Jamel <linuxmalaysia@songketmail.org>
2025-12-29 06:11:20 +0800

    chore: exclude sitemap scripts from auto-generated navigation

--- includes/nav-helper.inc.php
+++ includes/nav-helper.inc.php
@@ -8,13 +8,24 @@ declare(strict_types=1);
 if (!function_exists('get_site_pages')) {
     function get_site_pages(): array {
         $pages = [];
-        $exclude = ['template.php', 'index.php', 'audit-semantics.php', 'sitemap-page.php'];
+        // Added sitemap-page.php to the exclusion list
+        $exclude = [
+            'template.php', 
+            'index.php', 
+            'audit-semantics.php', 
+            'sitemap-page.php',
+            'sitemap.php'
+        ];
 
+        // Scan the root directory
         $files = scandir(__DIR__ . '/../');
 
         foreach ($files as $file) {
+            // Use the corrected constant: PATHINFO_EXTENSION
             if (pathinfo($file, PATHINFO_EXTENSION) === 'php' && !in_array($file, $exclude)) {
                 $name = pathinfo($file, PATHINFO_FILENAME);
+                
+                // Convert filename to a pretty label (e.g., 'about-us' to 'About Us')
                 $pages[$file] = ucfirst(str_replace(['-', '_'], ' ', $name));
             }
         }
