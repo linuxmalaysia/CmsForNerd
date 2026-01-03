4e47dda6aed2bf2c557ea92386fb920752125dda
Harisfazillah Jamel <linuxmalaysia@songketmail.org>
2026-01-04 07:21:14 +0800

    test: update security utility tests

--- tests/SecurityTest.php
+++ tests/SecurityTest.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
@@ -15,7 +16,7 @@ final class SecurityTest extends TestCase
     {
         $this->assertTrue(SecurityUtils::isValidPageName('about'));
         $this->assertTrue(SecurityUtils::isValidPageName('my-page_123'));
-        
+
         // Security checks
         $this->assertFalse(SecurityUtils::isValidPageName('../etc/passwd'), 'Directory traversal should fail');
         $this->assertFalse(SecurityUtils::isValidPageName('page.php'), 'Extensions should fail validation if not allowed');
@@ -30,7 +31,7 @@ final class SecurityTest extends TestCase
         // Known Bots
         $this->assertTrue(is_bot('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'));
         $this->assertTrue(is_bot('Mozilla/5.0 (compatible; Bingbot/2.0; +http://www.bing.com/bingbot.htm)'));
-        
+
         // Known Humans
         $this->assertFalse(is_bot('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'));
         $this->assertFalse(is_bot('Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1'));
